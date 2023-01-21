2cf961de04b421c38ee0f1c549e5fd5dd5172ebd
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-01-21 03:43:30 +0000

    Added MDX and tailwind integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://mbrdg.github.io',
+  integrations: [mdx(), tailwind()]
 });
