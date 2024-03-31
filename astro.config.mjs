7f6eefcb648d640090a6ded85c608ecabb986f06
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-03-31 04:55:38 +0100

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mbrdg.github.io',
   integrations: [
+    sitemap(),
     tailwind({
       nesting: true,
     }),
