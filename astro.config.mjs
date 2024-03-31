59dba5340b4dee46a1146a03e6855c8e4addd434
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-03-31 02:47:14 +0100

    chore: configuring typography and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mbrdg.github.io',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      nesting: true,
+    }),
+  ],
 });
