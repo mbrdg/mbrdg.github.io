f605e6806d0dd72c834cede86aa6e8b7a769a079
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-04-08 16:54:13 +0100

    feat: add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,27 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const blog = await getCollection("blog");
+
+  return rss({
+    title: "Coffee & State Synchronization",
+    description: `Interesting things about technology and software development`,
+    site: context.site,
+    items: blog.map(post => ({
+      title: post.data.title,
+      publication: post.data.publication,
+      description: post.data.description,
+      author: post.data.author,
+      tags: post.data.tags,
+      link: `/blog/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+    })),
+  });
+}
